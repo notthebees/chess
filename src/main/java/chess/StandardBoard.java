6e428aebbc8390d9dadc53b312257ac9fd5ac729
Paddy <pemcgovern@gmail.com>
2015-07-08 16:05:36 +0100

    Implement equals and toString in StandardBoard.

--- src/main/java/chess/StandardBoard.java
+++ src/main/java/chess/StandardBoard.java
@@ -1,11 +1,36 @@
 package chess;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
 import chess.pieces.Piece;
 
 public class StandardBoard implements Board {
 
+	private final List<Piece> pieces = new ArrayList<>();
+
 	public StandardBoard(final Piece...pieces) {
-		// TODO Auto-generated constructor stub
+		for (final Piece piece : pieces) {
+			this.pieces.add(piece);
+		}
+	}
+
+	@Override
+	public String toString() {
+		return "Standard Board with " + pieces.toString();
+	}
+
+	@Override
+	public boolean equals(final Object obj) {
+		return EqualsBuilder.reflectionEquals(this, obj);
+	}
+
+	@Override
+	public int hashCode() {
+		return HashCodeBuilder.reflectionHashCode(this);
 	}
 
 }
