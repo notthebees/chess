315353daeacb015db3f1183e37cf1164c0d7a26f
Paddy <pemcgovern@gmail.com>
2015-07-13 13:34:40 +0100

    Implement Pawn with basic move.

---
+++ src/main/java/chess/pieces/Pawn.java
@@ -0,0 +1,53 @@
+package chess.pieces;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
+public class Pawn implements Piece{
+
+	private final Colour colour;
+	private final Position position;
+
+	public Pawn(final Colour colour, final Position position) {
+		this.colour = colour;
+		this.position = position;
+	}
+
+	@Override
+	public Pawn moveTo(final Position position) {
+		if (canMoveTo(position)) {
+			return new Pawn(colour, position);
+		}
+		return this;
+	}
+
+	private boolean canMoveTo(final Position position) {
+		return (position.column == this.position.column + 1) & (position.row == this.position.row);
+	}
+
+	@Override
+	public Colour colour() {
+		return colour;
+	}
+
+	@Override
+	public Position position() {
+		return position;
+	}
+
+	@Override
+	public String toString() {
+		return colour.toString() + " Pawn @ " + position;
+	}
+
+	@Override
+	public boolean equals(final Object obj) {
+		return EqualsBuilder.reflectionEquals(this, obj);
+	}
+
+	@Override
+	public int hashCode() {
+		return HashCodeBuilder.reflectionHashCode(this);
+	}
+
+}
