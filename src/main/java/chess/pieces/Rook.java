6c8daee0a550777f3281e2f95471e32f81f2942b
Paddy <pemcgovern@gmail.com>
2015-07-13 18:31:19 +0100

    Add legality checking for Rook.

--- src/main/java/chess/pieces/Rook.java
+++ src/main/java/chess/pieces/Rook.java
@@ -1,5 +1,8 @@
 package chess.pieces;
 
+import static java.lang.Math.abs;
+import static java.lang.Math.signum;
+
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 
@@ -22,7 +25,54 @@ public class Rook implements Piece {
 
 	@Override
 	public boolean moveIsIllegal(final Position position, final Board board) {
-		return ! ((position.column == this.position.column) | (position.row == this.position.row));
+		if (! (verticalMove(position) | horizontalMove(position))) {
+			return true;
+		}
+		if (board.isOccupiedBy(colour, position)) {
+			return true;
+		}
+		if (routeIsNotClear(position, board)) {
+			return true;
+		}
+		return false;
+	}
+
+	private boolean horizontalMove(final Position position) {
+		return position.row == this.position.row;
+	}
+
+	private boolean verticalMove(final Position position) {
+		return position.column == this.position.column;
+	}
+
+	private boolean routeIsNotClear(final Position destination, final Board board) {
+		if (verticalMove(destination)) {
+			return verticalRouteIsNotClear(destination, board);
+		} else {
+			return horizontalRouteIsNotClear(destination, board);
+		}
+	}
+
+	private boolean horizontalRouteIsNotClear(final Position destination, final Board board) {
+		final int columnSign = (int) signum(destination.column - position.column);
+		for (int i = 1; i < abs(destination.column - position.column); i++) {
+			final Position positionOnRoute = new Position(position.column + i*columnSign, position.row);
+			if (board.isOccupiedAt(positionOnRoute)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private boolean verticalRouteIsNotClear(final Position destination, final Board board) {
+		final int rowSign = (int) signum(destination.row - position.row);
+		for (int i = 1; i < abs(destination.row - position.row); i++) {
+			final Position positionOnRoute = new Position(position.column, position.row + i*rowSign);
+			if (board.isOccupiedAt(positionOnRoute)) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 	@Override
--- src/test/java/chess/pieces/TestRook.java
+++ src/test/java/chess/pieces/TestRook.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import static chess.pieces.Colour.BLACK;
+import static chess.pieces.Colour.WHITE;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
@@ -12,6 +13,25 @@ import chess.StandardBoard;
 public class TestRook {
 
 	private final Board emptyBoard = new StandardBoard();
+
+	@Test
+	public void moveIllegalIfPathObstructedByAnyPiece() {
+		final Rook rook = new Rook(BLACK, new Position(4, 4));
+		final Board board = new StandardBoard(
+				rook,
+				new Pawn(BLACK, position(5,  4)),
+				new Pawn(WHITE, position(4,  2)));
+
+		assertThat(rook.moveIsIllegal(position(7, 4), board), equalTo(true));
+		assertThat(rook.moveIsIllegal(position(4, 1), board), equalTo(true));
+	}
+
+	@Test
+	public void moveIllegalIfDestinationOccupiedByFriendlyPiece() {
+		final Rook rook = new Rook(BLACK, new Position(1, 1));
+		final Board board = new StandardBoard(rook, new Pawn(BLACK, position(3,  1)));
+		assertThat(rook.moveIsIllegal(position(3, 1), board), equalTo(true));
+	}
 
 	@Test
 	public void doesntMoveIfIllegal() {
