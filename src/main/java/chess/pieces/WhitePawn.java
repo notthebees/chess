afa5ec4cf4b0616acce7be75dc0c922500248828
Paddy <pemcgovern@gmail.com>
2015-07-14 09:53:57 +0100

    Create subclasses of Pawn for each colour.

---
+++ src/main/java/chess/pieces/WhitePawn.java
@@ -0,0 +1,11 @@
+package chess.pieces;
+
+import static chess.pieces.Colour.WHITE;
+
+public class WhitePawn extends Pawn {
+
+	public WhitePawn(final Position position) {
+		super(WHITE, position);
+	}
+
+}
---
+++ src/test/java/chess/pieces/BlackPawn.java
@@ -0,0 +1,11 @@
+package chess.pieces;
+
+import static chess.pieces.Colour.BLACK;
+
+public class BlackPawn extends Pawn {
+
+	public BlackPawn(final Position position) {
+		super(BLACK, position);
+	}
+
+}
