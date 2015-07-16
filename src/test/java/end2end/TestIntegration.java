8851b4c4627838b62d0a5677afb96ba0325e8a8e
Paddy <pemcgovern@gmail.com>
2015-07-16 10:24:08 +0100

    New end2end test.

--- src/test/java/end2end/TestIntegration.java
+++ src/test/java/end2end/TestIntegration.java
@@ -12,33 +12,61 @@ import chess.ChessGame;
 import chess.Game;
 import chess.StandardBoardBuilder;
 import chess.pieces.King;
-import chess.pieces.Piece;
 import chess.pieces.Position;
+import chess.pieces.move.Move;
 import chess.pieces.move.SimpleMove;
 
 public class TestIntegration {
 
 	@Test
-	public void twoPlayersTakeTurnsMovingKings() {
-		final Piece whiteKing = new King(WHITE, position(5, 1));
-		final Piece blackKing = new King(BLACK, position(4, 8));
+	public void gameWithSomeMovesPlayedOutOfTurn() {
+		final Game game = new ChessGame(
+				board()
+				.withPieces(new King(WHITE, at(5, 1)), new King(BLACK, at(4, 8)))
+				.build());
 
-		final Board board = board().withPieces(whiteKing, blackKing).build();
+		game.play(move(from(4, 8), to(5, 8)));
+		game.play(move(from(5, 1), to(6, 2)));
+		game.play(move(from(6, 2), to(6, 1)));
+		game.play(move(from(4, 8), to(4, 7)));
 
-		final Game game = new ChessGame(board);
-
-		game.play(new SimpleMove(position(5, 1), position(6, 2)));
-		game.play(new SimpleMove(position(4, 8), position(5, 8)));
-
-		final Board finalBoard = board().withPieces(
-				new King(WHITE, position(6, 2)),
-				new King(BLACK, position(5, 8)))
+		final Board finalBoard = board()
+				.withPieces(new King(WHITE, at(6, 2)), new King(BLACK, at(4, 7)))
 				.build();
 
 		assertThat(game.currentBoard(), equalTo(finalBoard));
 	}
 
-	private Position position(final int column, final int row) {
+	@Test
+	public void twoPlayersTakeTurnsMovingKings() {
+		final Game game = new ChessGame(
+				board()
+				.withPieces(new King(WHITE, at(5, 1)), new King(BLACK, at(4, 8)))
+				.build());
+
+		game.play(move(from(5, 1), to(6, 2)));
+		game.play(move(from(4, 8), to(5, 8)));
+
+		final Board finalBoard = board()
+				.withPieces(new King(WHITE, at(6, 2)), new King(BLACK, at(5, 8)))
+				.build();
+
+		assertThat(game.currentBoard(), equalTo(finalBoard));
+	}
+
+	private Move move(final Position from, final Position to) {
+		return new SimpleMove(from, to);
+	}
+
+	private Position at(final int column, final int row) {
+		return new Position(column, row);
+	}
+
+	private Position to(final int column, final int row) {
+		return new Position(column, row);
+	}
+
+	private Position from(final int column, final int row) {
 		return new Position(column, row);
 	}
 
